497cb8d46e813af0110879c55b28f75e78c08d71
unknown <faruqabshar@gmail.com>
2020-11-23 21:11:35 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txt_profile_name;
+    Button btn_logout;
+
+    SharedPreferences sharedpreferences;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        getSupportActionBar().setElevation(0);
+
+        txt_profile_name = (TextView) findViewById(R.id.txt_profile_name);
+        txt_profile_name.setText(getIntent().getExtras().get("name").toString());
+
+        btn_logout = (Button) findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+}
